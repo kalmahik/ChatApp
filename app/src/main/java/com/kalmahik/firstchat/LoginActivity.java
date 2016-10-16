7d41b9cee1538360ca4ccc620f6b801e1a69abe8
kalmahik <kalmahik@yahoo.com>
2016-10-16 11:49:55 +0300

    6

--- app/src/main/java/com/kalmahik/firstchat/LoginActivity.java
+++ app/src/main/java/com/kalmahik/firstchat/LoginActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView registerRef;
+    private Button signInButton;
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,32 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);            }
+                startActivity(intent);
+            }
         });
+
+        signInButton = (Button) findViewById(R.id.button_login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correctSignIn();
+            }
+        });
+
+    }
+
+    private void correctSignIn() {
+        if (username.length() > 0) {
+            if (password.length() > 0) {
+                Toast.makeText(this, "Login success", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Invalid password or username", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Invalid password or usernam", Toast.LENGTH_SHORT).show();
+        }
     }
 }
