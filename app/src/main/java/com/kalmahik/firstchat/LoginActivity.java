47eb32712db210c66d9c358a560aeff42a707d0d
kalmahik <kalmahik@yahoo.com>
2016-10-16 11:36:15 +0300

    5

--- app/src/main/java/com/kalmahik/firstchat/LoginActivity.java
+++ app/src/main/java/com/kalmahik/firstchat/LoginActivity.java
@@ -1,11 +1,9 @@
 package com.kalmahik.firstchat;
 
 import android.content.Intent;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
